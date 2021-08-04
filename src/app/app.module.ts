45488bd75cd7a9c6bfe3698586ff14ba1f54bb83
unknown <rineeshkumar9@gmail.com>
2021-08-04 12:27:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "@angular/fire/auth";
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAYGcymd2SgNxnoBsmHhEK5ZMZgcNmUwqQ",
+  authDomain: "iq-5bb8a.firebaseapp.com",
+  databaseURL: "https://iq-5bb8a-default-rtdb.firebaseio.com",
+  projectId: "iq-5bb8a",
+  storageBucket: "iq-5bb8a.appspot.com",
+  messagingSenderId: "783920765544",
+  appId: "1:783920765544:web:b9cc488d3588affd5879dd",
+  measurementId: "G-XPP04Q5P4C"
+}
 };
